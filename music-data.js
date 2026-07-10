7adcbff5ae38f014b3833b1bfb886c6986d08a2f
Yuvraj Ab <yuvraajapte@gmail.com>
2026-07-10 15:59:29 +0530

    Add files via upload

---
+++ music-data.js
@@ -0,0 +1,128 @@
+const MUSIC_LIBRARY = [
+  {
+    id: "ashiqui-2",
+    title: "Ashiqui 2",
+    cover: "ashiqui.png",
+    songs: [
+      {
+        title: "Asaan Nhi Yahan",
+        artist: "Ashiqui 2",
+        thumb: "ashiqui.png",
+        file: "musics/Aasaan Nhi Yahan.mp3"
+      },
+      {
+        title: "Chahun Mai ya na",
+        artist: "Ashiqui 2",
+        thumb: "ashiqui.png",
+        file: "musics/Chahun Mai Ya Na.mp3"
+      }
+    ]
+  },
+  {
+    id: "p-pop-culture",
+    title: "P-Pop Culture",
+    cover: "ppop.png",
+    songs: [
+      {
+        title: "MF Gabhru!",
+        artist: "Karan Aujla",
+        thumb: "ppop.png",
+        file: "musics/MF Gabhru!.mp3"
+      },
+      {
+        title: "7.7 Magnitude",
+        artist: "Karan Aujla",
+        thumb: "ppop.png",
+        file: "musics/7.7 Magnitude.mp3"
+      },
+      {
+        title: "Boyfriend",
+        artist: "Karan Aujla",
+        thumb: "ppop.png",
+        file: "musics/Boyfriend.mp3"
+      },
+      {
+        title: "For A Reason",
+        artist: "Karan Aujla",
+        thumb: "ppop.png",
+        file: "musics/For A Reason.mp3"
+      },
+      {
+        title: "Daytona",
+        artist: "Karan Aujla",
+        thumb: "ppop.png",
+        file: "musics/Daytona.mp3"
+      },
+      {
+        title: "Youre U Tho",
+        artist: "Karan Aujla",
+        thumb: "ppop.png",
+        file: "musics/Youre U Tho.mp3"
+      },
+      {
+        title: "P-Pop Cultue",
+        artist: "Karan Aujla",
+        thumb: "ppop.png",
+        file: "musics/P-POP CULTURE.mp3"
+      },
+      {
+        title: "Flip-Side (Sandys Interlude)",
+        artist: "Karan Aujla",
+        thumb: "ppop.png",
+        file: "musics/Flip-Side (Sandys Interlude).mp3"
+      },
+      {
+        title: "I Really Do...",
+        artist: "Karan Aujla",
+        thumb: "ppop.png",
+        file: "musics/I Really do....mp3"
+      },
+      {
+        title: "Ima Do My Thiiing",
+        artist: "Karan Aujla",
+        thumb: "ppop.png",
+        file: "musics/Ima Do My Thiiing.mp3"
+      },
+      {
+        title: "HIM",
+        artist: "Karan Aujla",
+        thumb: "ppop.png",
+        file: "musics/HIM..mp3"
+      }
+    ]
+  },
+  {
+    id: "punk",
+    title: "Punk",
+    cover: "https://images.unsplash.com/photo-1598387993441-a364f854c3e1?w=700&q=80",
+    songs: [
+      {
+        title: "Riot Girl",
+        artist: "Static Youth",
+        file: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-6.mp3"
+      },
+      {
+        title: "Broken Chains",
+        artist: "Static Youth",
+        file: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-7.mp3"
+      }
+    ]
+  },
+  {
+    id: "jazz",
+    title: "Jazz",
+    cover: "https://images.unsplash.com/photo-1511192336575-5a79af67a629?w=700&q=80",
+    songs: [
+      {
+        title: "Blue Hour",
+        artist: "Marcus Cole",
+        file: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3"
+      },
+      {
+        title: "Smoke & Mirrors",
+        artist: "Marcus Cole",
+        file: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3"
+      }
+    ]
+  }
+];
