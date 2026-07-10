2a00bde060093c71c305436e5dbf09550fb5c0f3
Yuvraj Ab <yuvraajapte@gmail.com>
2026-07-10 12:40:01 +0530

    Update music-data.js

--- music-data.js
+++ music-data.js
@@ -2,37 +2,37 @@ const privateTracklist = [
     {
         title: "Asaan Nhi Yahan",
         artist: "Ashiqui 2",
-        thumb: "musics/covers/kahani-suno.jpg",
+        thumb: "Ashiqui.png",
         file: "musics/Aasaan Nhi Yahan.mp3"
     },
     {
         title: "Chahun Mai ya na",
         artist: "Ashiqui 2",
-        thumb: "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?w=500", // Online link sample
+        thumb: "Ashiqui.png",
         file: "musics/Chahun Mai Ya Na.mp3"
     },
         {
         title: "MF Gabhru!",
         artist: "Karan Aujla",
-        thumb: "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?w=500", // Online link sample
+        thumb: "ppop.png",
         file: "musics/MF Gabhru!.mp3"
     },
     {
         title: "7.7 Magnitude",
         artist: "Karan Aujla",
-        thumb: "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?w=500", // Online link sample
+        thumb: "ppop.png",
         file: "musics/7.7 Magnitude.mp3"
     },
     {
         title: "Boyfriend",
         artist: "Karan Aujla",
-        thumb: "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?w=500", // Online link sample
+        thumb: "ppop.png",
         file: "musics/Boyfriend.mp3"
     },
     {
         title: "For A Reason",
         artist: "Karan Aujla",
-        thumb: "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?w=500", // Online link sample
+        thumb: "ppop.png",
         file: "musics/For A Reason.mp3"
     }
 ];
